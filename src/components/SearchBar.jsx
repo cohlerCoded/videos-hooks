a2971cf808861584ddffe5b33f616802126b7fa3
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-14 12:05:25 -0700

    searchbar setup

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  state = { term: "" };
+  onInputChange = () => {};
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="ui segment search-bar">
+        <form className="ui category search" action="" preventDefault>
+          <label htmlFor="search">Search Videos: </label>
+          <div class="ui icon input">
+            <input
+              class="prompt"
+              type="search"
+              name="search"
+              id="search"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+            <i class="search icon"></i>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
