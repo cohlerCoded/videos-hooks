f811dc1a851e3449ffc0add845fedb32d6faac69
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-14 14:21:23 -0700

    rendering video thumbnails

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import VideoItem from "./VideoItem";
 
 export default function VideoList({ videos }) {
-  const renderedList = videos.map((video) => <VideoItem />);
+  const renderedList = videos.map((video) => <VideoItem video={video} />);
   return <div>{renderedList}</div>;
 }
